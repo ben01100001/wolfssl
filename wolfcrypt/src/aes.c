72174ff1603b6d8ac5ae473231f223088eefe5fd
ambiso <ambisotopy@gmail.com>
2023-11-14 18:31:58 +0100

    Prototype using hardware AES

--- wolfcrypt/src/aes.c
+++ wolfcrypt/src/aes.c
@@ -3023,6 +3023,40 @@ int wc_AesSetIV(Aes* aes, const byte* iv)
 }
 
 /* AES-DIRECT */
+#define AES      ((Aes_Reg*)(0xF00B0000))
+
+#include <stdint.h>
+
+typedef struct
+{
+  volatile uint32_t STATUS;
+  volatile uint32_t CTRL;
+  volatile uint32_t KEY;
+  volatile uint32_t PLAIN;
+  volatile uint32_t CIPHER;
+} Aes_Reg;
+
+void AES_write(Aes_Reg *reg, uint32_t *key, uint8_t *plain) {
+    uint32_t plain_data = 0;
+    for (int i = 0; i < 16; i++) {
+      plain_data = (plain_data<<8) | plain[i];
+      if ((i&3) == 3) {
+        reg -> KEY = key[i>>2];
+        reg -> PLAIN = plain_data;
+      }
+    }
+}
+
+void AES_read(Aes_Reg *reg, uint8_t *cipher) {
+    for (int i = 0; i < 4; i++) {
+      uint32_t cipher_data = reg -> CIPHER;
+      cipher[(i<<2) + 3] = (cipher_data >>  0) & 0xff;
+      cipher[(i<<2) + 2] = (cipher_data >>  8) & 0xff;
+      cipher[(i<<2) + 1] = (cipher_data >> 16) & 0xff;
+      cipher[(i<<2) + 0] = (cipher_data >> 24) & 0xff;
+    }
+}
+
 #if defined(WOLFSSL_AES_DIRECT)
     #if defined(HAVE_COLDFIRE_SEC)
         #error "Coldfire SEC doesn't yet support AES direct"
@@ -3040,7 +3074,33 @@ int wc_AesSetIV(Aes* aes, const byte* iv)
         /* Allow direct access to one block encrypt */
         void wc_AesEncryptDirect(Aes* aes, byte* out, const byte* in)
         {
-            wc_AesEncrypt(aes, in, out);
+            if (aes->keylen == 16) {
+                println("with key len 16");
+                AES_write(AES, (byte*)aes->key, in);
+                AES->CTRL |= 1;
+                while(AES->STATUS != 1){
+                    asm volatile ("nop");
+                }
+                byte buf[16];
+                AES_read(AES, buf);
+                wc_AesEncrypt(aes, in, out);
+                for(int i = 0; i < 16; ++i) {
+                    if (buf[i] != out[i]) {
+                        println("mismatch");
+                        break;
+                    }
+                }
+                phex("key", aes->key, 16);
+                phex("in", in, 16);
+                phex("out", out, 16);
+                phex("buf", buf, 16);
+            } else {
+                println("with key len != 16");
+                for (int i = 0; i < aes->keylen; ++i) {
+                    println("asdf");
+                }
+                wc_AesEncrypt(aes, in, out);
+            }
         }
         #ifdef HAVE_AES_DECRYPT
         /* Allow direct access to one block decrypt */
